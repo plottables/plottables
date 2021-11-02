41cd1ccd89e7e4dd2b5e7ad84997e8cb58215c94
Matt Jacobson <matthew.g.jacobson@gmail.com>
2021-11-01 21:39:13 -0400

    adding style and wallet check to plot view

--- pages/token/[tokenId]/plot.tsx
+++ pages/token/[tokenId]/plot.tsx
@@ -1,24 +1,48 @@
+import { useWalletContext } from "@/components/common/WalletProvider";
+import Container from "@/components/Container";
+import { ownerOf } from "@/lib/coreContract";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export default function Plot(props: { svg: any }) {
+export default function Plot(props: { owner: string }) {
   const router = useRouter();
   const { tokenId } = router.query;
+  const walletAddress = useWalletContext();
+  const [showState, setShowState] = useState(false);
+  useEffect(() => {
+    if (walletAddress.toLowerCase() === props.owner.toLowerCase()) {
+      setShowState(true);
+    }
+  }, [walletAddress, props.owner]);
 
-  return (
-    <div style={{ padding: "20px" }}>
-      <div>Token: {tokenId}</div>
-      <div style={{ border: "3px solid blue" }}>
-        {tokenId && (
-          <iframe
-            src={`/api/tokens/${tokenId}/plot`}
-            style={{
-              height: "calc(100vh - 80px)",
-              width: "100%",
-              border: 0,
-            }}
-          />
-        )}
+  if (showState) {
+    return (
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <div style={{ width: "100%", height: "100%" }}>
+          {
+            <iframe
+              src={`/api/tokens/${tokenId}/plot`}
+              style={{
+                height: "100%",
+                width: "100%",
+                border: 0,
+              }}
+            />
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <Container>please connect as {props.owner} to plot</Container>;
+  }
 }
+
+export const getServerSideProps: (context: any) => Promise<{ props: any }> =
+  async (context) => {
+    const tokenId = context.params?.tokenId;
+    return {
+      props: {
+        owner: await ownerOf(tokenId),
+      },
+    };
+  };
