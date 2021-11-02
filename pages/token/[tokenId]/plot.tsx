af1ee2e2f3bdd818aac047ef7b8ab92a0e3adc26
Matt Jacobson <matthew.g.jacobson@gmail.com>
2021-11-01 22:28:52 -0400

    cleaning up ux for plot view reconnect

--- pages/token/[tokenId]/plot.tsx
+++ pages/token/[tokenId]/plot.tsx
@@ -1,6 +1,7 @@
 import { useWalletContext } from "@/components/common/WalletProvider";
-import Container from "@/components/Container";
 import { ownerOf } from "@/lib/coreContract";
+import styles from "@/styles/Plot.module.css";
+import { connectWallet } from "@/utils/interact";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -9,32 +10,48 @@ export default function Plot(props: { owner: string }) {
   const { tokenId } = router.query;
   const walletAddress = useWalletContext();
   const [showState, setShowState] = useState(false);
+
   useEffect(() => {
     if (walletAddress.toLowerCase() === props.owner.toLowerCase()) {
       setShowState(true);
+    } else {
+      setShowState(false);
     }
   }, [walletAddress, props.owner]);
 
-  if (showState) {
-    return (
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <div style={{ width: "100%", height: "100%" }}>
-          {
-            <iframe
-              src={`/api/tokens/${tokenId}/plot`}
-              style={{
-                height: "100%",
-                width: "100%",
-                border: 0,
-              }}
-            />
-          }
+  const reconnect = async () => {
+    if (walletAddress.length != 0) {
+      alert("please disconnect first");
+    } else {
+      await connectWallet();
+    }
+  };
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div
+        className={styles.overlay}
+        style={{
+          display: showState ? "none" : "block",
+        }}
+      >
+        <div onClick={reconnect}>
+          click here to reconnect as {props.owner.toLowerCase().substring(0, 8)}
         </div>
       </div>
-    );
-  } else {
-    return <Container>please connect as {props.owner} to plot</Container>;
-  }
+      <div style={{ width: "100%", height: "100%" }}>
+        <iframe
+          src={`/api/tokens/${tokenId}/plot`}
+          style={{
+            height: "100%",
+            width: "100%",
+            border: 0,
+            pointerEvents: showState ? "auto" : "none",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps: (context: any) => Promise<{ props: any }> =
