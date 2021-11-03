6f2d49dc6b5c0c94a2a1b46687a40d6c97a6dac9
Matt Jacobson <matthew.g.jacobson@gmail.com>
2021-11-03 16:11:45 -0400

    updating the base url for images

--- config/index.js
+++ config/index.js
@@ -5,8 +5,7 @@ export const coreContractAddress = "0xd10e3DEe203579FcEE90eD7d0bDD8086F7E53beB";
 export const minterContractAddress =
   "0x17D34c3C10eDE845D1875eA0Db4293BFEd9d4971";
 
-export const host = "https://nextjs-flame-gamma-79.vercel.app/";
-export const imageBaseUrl = "https://d2ekshiy7r5vl7.cloudfront.net/";
+export const imageBaseUrl = "https://plottables-staging.s3.amazonaws.com/";
 export const liveBaseUrl = "https://generator-staging.artblocks.io/";
 
 export { coreAbi, minterAbi };
