8571a30e9144709a1dd1c9013f27ff1972fe0edd
Matt Jacobson <matthew.g.jacobson@gmail.com>
2021-11-06 19:13:51 -0400

    adding line breaks to gallery page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -9,9 +9,40 @@ interface GalleryProps {
   projects: any[];
 }
 
+function makeBreak() {
+  let path = "M 0 " + (10 * Math.random() + 5);
+  for (let i = 1; i < 100; i++) {
+    path +=
+      " S " +
+      (i - 0.5) +
+      " " +
+      (10 * Math.random() + 5) +
+      " " +
+      i +
+      " " +
+      (10 * Math.random() + 5);
+  }
+  return (
+    <svg
+      preserveAspectRatio="none"
+      width="100%"
+      height="17px"
+      viewBox="0 0 100 20"
+    >
+      <path
+        style={{ fill: "none", strokeWidth: "2", stroke: "rgb(0%,0%,0%)" }}
+        d={path}
+        vectorEffect="non-scaling-stroke"
+      />
+    </svg>
+  );
+}
+
 export default function Gallery({ projects }: GalleryProps) {
   return (
     <Container>
+      {makeBreak()}
+      <br />
       <br />
       {projects.map((p) => {
         let randomToken =
@@ -49,6 +80,11 @@ export default function Gallery({ projects }: GalleryProps) {
                 />
               </div>
             </div>
+            <br />
+            <br />
+            {makeBreak()}
+            <br />
+            <br />
           </div>
         );
       })}
