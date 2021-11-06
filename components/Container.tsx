c08f2f4522d12040eacf81581a717bcf51f49fe6
Matt Jacobson <matthew.g.jacobson@gmail.com>
2021-11-06 19:20:41 -0400

    adding title to container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,10 +1,15 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from "next/head";
 import React from "react";
 
 const Container: React.FC = ({ children }) => {
   return (
     <div className="app">
+      <Head>
+        <title>plottables</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div id="paper">
         <div id="pattern">
           <div id="content">
