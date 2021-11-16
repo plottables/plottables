87b262381174a2e6f9e537b15cf2fb97e6a643eb
mattjacobson <matthew.g.jacobson@gmail.com>
2021-11-16 16:59:23 -0500

    adding open filter to gallery page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -5,61 +5,90 @@ import { makeLineBreak } from "@/lib/makeLineBreak";
 import styles from "@/styles/Gallery.module.css";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
+import { useState } from "react";
 
 interface GalleryProps {
   projects: any[];
 }
 
 export default function Gallery({ projects }: GalleryProps) {
+  const [filter, setFilter] = useState("all");
   return (
     <Container>
+      <div className={styles.selectorContainer}>
+        <div
+          className={filter === "all" ? styles.highlight : undefined}
+          onClick={() => setFilter("all")}
+        >
+          all projects
+        </div>
+        <div
+          className={filter === "open" ? styles.highlight : undefined}
+          onClick={() => setFilter("open")}
+        >
+          open projects
+        </div>
+      </div>
+      <br />
       {makeLineBreak()}
       <br />
       <br />
-      {projects.map((p) => {
-        let randomToken =
-          1000000 * p.projectId +
-          Math.floor(Math.random() * p.projectTokenInfo.invocations);
-        return (
-          <div key={p.projectId}>
-            <Link href={`/project/${p.projectId}`}>
-              <a>
-                {p.projectDetails.projectName} by {p.projectDetails.artist}
-              </a>
-            </Link>
-            <div className={styles.projectContainer}>
-              <div className={styles.projectDetails}>
-                <br />
-                {p.projectDetails.description}
-                <br />
-                <br />
-                Total Minted: {p.projectTokenInfo.invocations} /{" "}
-                {p.projectTokenInfo.maxInvocations}
-                <br />
-                <br />
-                Price per token:{" "}
-                {p.projectTokenInfo.pricePerTokenInWei / 1000000000000000000}
-                <br />
-                <br />
+      {projects
+        .filter((p) => p.projectTokenInfo.active)
+        .filter((p) => {
+          if (filter == "open") {
+            return (
+              parseInt(p.projectTokenInfo.invocations) <
+              parseInt(p.projectTokenInfo.maxInvocations)
+            );
+          } else {
+            return true;
+          }
+        })
+        .map((p) => {
+          let randomToken =
+            1000000 * p.projectId +
+            Math.floor(Math.random() * p.projectTokenInfo.invocations);
+          return (
+            <div key={p.projectId}>
+              <Link href={`/project/${p.projectId}`}>
+                <a>
+                  {p.projectDetails.projectName} by {p.projectDetails.artist}
+                </a>
+              </Link>
+              <div className={styles.projectContainer}>
+                <div className={styles.projectDetails}>
+                  <br />
+                  {p.projectDetails.description}
+                  <br />
+                  <br />
+                  Total Minted: {p.projectTokenInfo.invocations} /{" "}
+                  {p.projectTokenInfo.maxInvocations}
+                  <br />
+                  <br />
+                  Price per token:{" "}
+                  {p.projectTokenInfo.pricePerTokenInWei / 1000000000000000000}
+                  <br />
+                  <br />
+                </div>
+                <div className={styles.projectPreview}>
+                  <Link href={"/token/" + randomToken}>
+                    <a>#{randomToken - 1000000 * p.projectId}</a>
+                  </Link>
+                  <img
+                    src={imageBaseUrl + randomToken + ".png"}
+                    alt={"a random image"}
+                  />
+                </div>
               </div>
-              <div className={styles.projectPreview}>
-                <Link href={"/token/" + randomToken}>
-                  <a>#{randomToken - 1000000 * p.projectId}</a>
-                </Link>
-                <img
-                  src={imageBaseUrl + randomToken + ".png"}
-                  alt={"a random image"}
-                />
-              </div>
+              <br />
+              <br />
+              {makeLineBreak()}
+              <br />
+              <br />
             </div>
-            <br />
-            <br />
-            {makeLineBreak()}
-            <br />
-            <br />
-          </div>
-        );
-      })}
+          );
+        })}
     </Container>
   );
 }
