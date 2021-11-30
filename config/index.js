971589f7facb56c0912adae56b93d6b4e3b12092
mattjacobson <matthew.g.jacobson@gmail.com>
2021-11-30 10:13:52 -0700

    updating for mainnet contracts

--- config/index.js
+++ config/index.js
@@ -1,12 +1,24 @@
 import coreAbi from "./coreAbi.json";
 import minterAbi from "./minterAbi.json";
 
-export const coreContractAddress = "0xd10e3DEe203579FcEE90eD7d0bDD8086F7E53beB";
-export const minterContractAddress =
-  "0x17D34c3C10eDE845D1875eA0Db4293BFEd9d4971";
+export const coreContractAddress =
+  process.env.NEXT_PUBLIC_ETH_NETWORK === "main"
+    ? "0xa319C382a702682129fcbF55d514E61a16f97f9c"
+    : "0xd10e3DEe203579FcEE90eD7d0bDD8086F7E53beB";
 
-export const imageBaseUrl = "https://plottables-staging.s3.amazonaws.com/";
+export const minterContractAddress =
+  process.env.NEXT_PUBLIC_ETH_NETWORK === "main"
+    ? "0x463B8CED7D22a55Aa4A5d69EF6a54a08AA0feB93"
+    : "0x17D34c3C10eDE845D1875eA0Db4293BFEd9d4971";
+
+export const imageBaseUrl =
+  process.env.NEXT_PUBLIC_ETH_NETWORK === "main"
+    ? "https://plottables.s3.amazonaws.com/"
+    : "https://plottables-staging.s3.amazonaws.com/";
+
 export const liveBaseUrl =
-  "https://generator-staging.artblocks.io/0xd10e3dee203579fcee90ed7d0bdd8086f7e53beb/";
+  process.env.NEXT_PUBLIC_ETH_NETWORK === "main"
+    ? "https://generator.artblocks.io/0xa319C382a702682129fcbF55d514E61a16f97f9c/"
+    : "https://generator-staging.artblocks.io/0xd10e3dee203579fcee90ed7d0bdd8086f7e53beb/";
 
 export { coreAbi, minterAbi };
