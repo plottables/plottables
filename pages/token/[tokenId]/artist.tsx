fe2b3e1bd20cad43cde8365c6a944d900fe1ed9b
mattjacobson <matthew.g.jacobson@gmail.com>
2022-05-16 18:09:06 -0400

    adding signing functionality

--- pages/token/[tokenId]/artist.tsx
+++ pages/token/[tokenId]/artist.tsx
@@ -3,24 +3,22 @@ import {
   projectIdToArtistAddress,
   tokenIdToProjectId,
 } from "@/lib/coreContract";
-import {useRouter} from "next/router";
-import styles from "@/styles/Plot.module.css";
+import { useRouter } from "next/router";
 
 export default function Artist(props: { artistAddress: string }) {
-
   const router = useRouter();
   const { tokenId } = router.query;
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <iframe
-          src={`https://iammattjacobson.com/plotShareArtist/?url=https%3A%2F%2Fplottables-mainnet.s3.amazonaws.com%2F${tokenId}.png`}
-          style={{
-            height: "100%",
-            width: "100%",
-            border: 0
-          }}
-          allow="camera; microphone"
+        src={`https://iammattjacobson.com/plotShareArtist/?url=https%3A%2F%2Fplottables-mainnet.s3.amazonaws.com%2F${tokenId}.png`}
+        style={{
+          height: "100%",
+          width: "100%",
+          border: 0,
+        }}
+        allow="camera; microphone"
       />
     </div>
   );
--- pages/token/[tokenId]/collector.tsx
+++ pages/token/[tokenId]/collector.tsx
@@ -3,38 +3,36 @@ import {
   projectIdToArtistAddress,
   tokenIdToProjectId,
 } from "@/lib/coreContract";
-import {useRouter} from "next/router";
-import styles from "@/styles/Plot.module.css";
+import { useRouter } from "next/router";
 
 export default function Collector(props: { artistAddress: string }) {
-
   const router = useRouter();
   const { peerId } = router.query;
 
   return (
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <iframe
-            src={`https://iammattjacobson.com/plotShareCollector/?peerId=${peerId}`}
-            style={{
-              height: "100%",
-              width: "100%",
-              border: 0
-            }}
-            allow="camera; microphone"
-        />
-      </div>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <iframe
+        src={`https://iammattjacobson.com/plotShareCollector/?peerId=${peerId}`}
+        style={{
+          height: "100%",
+          width: "100%",
+          border: 0,
+        }}
+        allow="camera; microphone"
+      />
+    </div>
   );
 }
 
 export const getServerSideProps: (context: any) => Promise<{ props: any }> =
-    async (context) => {
-      const tokenId = context.params?.tokenId;
-      const projectId = await tokenIdToProjectId(tokenId);
-      return {
-        props: {
-          owner: await ownerOf(tokenId),
-          projectId: projectId,
-          artistAddress: await projectIdToArtistAddress(projectId),
-        },
-      };
+  async (context) => {
+    const tokenId = context.params?.tokenId;
+    const projectId = await tokenIdToProjectId(tokenId);
+    return {
+      props: {
+        owner: await ownerOf(tokenId),
+        projectId: projectId,
+        artistAddress: await projectIdToArtistAddress(projectId),
+      },
     };
+  };
