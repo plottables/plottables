5947f4bfd9823fbd76a2c0959c98a6714af63af4
matthewjacobson <matthew.g.jacobson@gmail.com>
2022-05-20 12:02:33 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ export const calendar = {
   8: "3/2/2022 2:00:00 PM EST",
   9: "3/30/2022 3:00:00 PM EDT",
   10: "5/5/2022 5:00:00 PM EDT",
-  10: "5/25/2022 2:00:00 PM EDT",
+  11: "5/25/2022 2:00:00 PM EDT",
 };
 
 export const darkblockProjects = [8];
